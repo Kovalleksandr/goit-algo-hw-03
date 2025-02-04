b61c4bbd256c11e86307f5ffa64a2352fe069188
OLEKSANDR KOVAL <koval.aleksandr.v@gmail.com>
2025-02-04 20:23:42 +0200

    new solution _task_2

--- home_work_3_2.py
+++ home_work_3_2.py
@@ -1,21 +1,29 @@
 import random
 
 def get_numbers_ticket(min: int, max: int, quantity: int):
-    if not (1 <= min <= 1000) or not (1 <= max <= 1000) or not (min <= quantity <= max):
+    if not (1 <= min <= 1000) or not (1 <= max <= 1000) or not (min <= max) or not (min <= quantity <= max):
         return []
     numbers = random.sample(range(min, max + 1), quantity)
 
     return sorted(numbers)
 
 print(get_numbers_ticket(1, 49, 6))
+print(get_numbers_ticket(60, 1, 5))
 
 '''
+Додав перевірку (min <= max)
+--------------------------------------------
+
+
+
 Навіщо перевірка (min <= quantity <= max)???
 --------------------------------------------
 Ця перевірка гарантує, що quantity (кількість чисел, які треба вибрати) знаходиться в межах [min, max].
 Наприклад, якщо користувач введе get_numbers_ticket(1, 10, 15),
 то це некоректний запит, адже в діапазоні від 1 до 10 всього 10 чисел, а вибрати потрібно 15.
 Перевірка min <= quantity <= max поверне False, і функція поверне [].
+
+
 
 
 Навіщо max + 1???
